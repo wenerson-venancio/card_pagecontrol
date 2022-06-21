517f0af967d55279fea449a64bac24753b6fcd64
Wenerson Venâncio <wenersonvenancio@MacBook-Pro-de-Wenerson-2.local>
2022-06-21 00:35:14 -0300

    Contador

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:flutter/material.dart';
-import './TelaHome.dart';
+
+import './app_widget.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Tela Home',
-      home: TelaHome(),
-    );
-  }
+  runApp(const AppWidget());
 }
